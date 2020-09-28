58bbe228fa89138a2361e5b30eeb2c6e603d0c2b
Irina Fylyppova (via Travis CI) <irina.h.fylyppova@gmail.com>
2020-09-28 12:42:42 +0000

    Deploy garevna/dgtek-geoscape to github.com/garevna/dgtek-geoscape.git:gh-pages

---
+++ precache-manifest.9728994ec55a11c0912d3a69c5c74c21.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba5681ecbff04c3380f84647255a547b",
+    "url": "/android-chrome-192x192.png"
+  },
+  {
+    "revision": "408c189194f146d27e526f0c08cb02d1",
+    "url": "/android-chrome-512x512.png"
+  },
+  {
+    "revision": "7d13462d8387c50a1d957b56c87e9058",
+    "url": "/apple-touch-icon.png"
+  },
+  {
+    "revision": "591ff276645a7b32de00",
+    "url": "/css/app.4caf4037.css"
+  },
+  {
+    "revision": "39e171f8c1a040040b42",
+    "url": "/css/chunk-vendors.30f1b444.css"
+  },
+  {
+    "revision": "6de038a7ee6eb1278cb13ae663c52c7f",
+    "url": "/favicon-16x16.png"
+  },
+  {
+    "revision": "654f0af686d9d3c49692977f38ea07f7",
+    "url": "/favicon-32x32.png"
+  },
+  {
+    "revision": "32750b00d6f5003097a67377a4af208f",
+    "url": "/fonts/Gilroy-Bold.32750b00.woff"
+  },
+  {
+    "revision": "3cf0ee273a0b3f022234b6572c3b78f9",
+    "url": "/fonts/Gilroy-Bold.3cf0ee27.ttf"
+  },
+  {
+    "revision": "5e111ca086414e1d8905e5a875fb6899",
+    "url": "/fonts/Gilroy-Bold.5e111ca0.eot"
+  },
+  {
+    "revision": "335c7ebfe69e97fec5ae32fea4fd17fa",
+    "url": "/fonts/Gilroy-Heavy.335c7ebf.eot"
+  },
+  {
+    "revision": "8d36efeb3349af073647c0c37ec995f1",
+    "url": "/fonts/Gilroy-Heavy.8d36efeb.ttf"
+  },
+  {
+    "revision": "ffa5800ec1a44bef80af3dfdced37e7a",
+    "url": "/fonts/Gilroy-Heavy.ffa5800e.woff"
+  },
+  {
+    "revision": "6444f14adcdee041b62184f13139a56d",
+    "url": "/fonts/Gilroy-Medium.6444f14a.ttf"
+  },
+  {
+    "revision": "7d5dc1791cf2738d55a87e871549004c",
+    "url": "/fonts/Gilroy-Medium.7d5dc179.woff"
+  },
+  {
+    "revision": "ce12e7f23ff25c6f534364a7435885df",
+    "url": "/fonts/Gilroy-Medium.ce12e7f2.eot"
+  },
+  {
+    "revision": "59525ce19c9e4999bc19d96e4a88c1b3",
+    "url": "/fonts/Gilroy-Regular.59525ce1.eot"
+  },
+  {
+    "revision": "8e8705d069fbb9099b3a1e1d779fa5c5",
+    "url": "/fonts/Gilroy-Regular.8e8705d0.woff"
+  },
+  {
+    "revision": "ae5e7255973ffe09b53f07a2805232a8",
+    "url": "/fonts/Gilroy-Regular.ae5e7255.ttf"
+  },
+  {
+    "revision": "484e8bd1865956810488fe9ce47d2d86",
+    "url": "/img/logo.484e8bd1.png"
+  },
+  {
+    "revision": "df5857453f4c52e6842627e421253d5d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d8370d860c7277bb20a6",
+    "url": "/js/about.4f5bae19.js"
+  },
+  {
+    "revision": "591ff276645a7b32de00",
+    "url": "/js/app.72b3ae0b.js"
+  },
+  {
+    "revision": "39e171f8c1a040040b42",
+    "url": "/js/chunk-vendors.6d76c362.js"
+  },
+  {
+    "revision": "b069197f28c779542b027568805b8b36",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "912d7b3836eeea0c1b59f7f724cc3270",
+    "url": "/mstile-150x150.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "cfb9277d45e54434ec544882d9447b5f",
+    "url": "/safari-pinned-tab.svg"
+  }
+]);
